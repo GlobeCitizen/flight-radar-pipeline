077b31078d4a5ab9c56ad1024076319cec1b7029
GlobeCitizen <ahnich97@gmail.com>
2024-04-12 10:58:27 +0200

    make a cli app with typer for answers

--- src/answers.py
+++ src/answers.py
@@ -1,7 +1,10 @@
-import glob, math, os, json
+import glob, os, json
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
-from pyspark.sql.types import FloatType
+from pyspark.sql.window import Window
+import typer
+
+app = typer.Typer()
 
 def create_spark_session():
     spark_master_url = "local[*]"
@@ -13,70 +16,7 @@ def create_spark_session():
         .getOrCreate()
     return spark
 
-def get_distance(lat1, lon1, lat2, lon2):
-    """
-    Calculate the distance between two points given their latitude and longitude in kilometers.
-
-    :param lat1: float
-    :param lon1: float
-    :param lat2: float
-    :param lon2: float
-    :return: float
-    """
-
-    if lat1 is None or lon1 is None or lat2 is None or lon2 is None:
-        return None
-    
-    # cast all values to float if it's possible
-    lat1, lon1, lat2, lon2 = map(float, [lat1, lon1, lat2, lon2])
-
-    # Convert latitude and longitude from degrees to radians
-    lat1, lon1, lat2, lon2 = map(
-        lambda x: math.radians(x),
-        [lat1, lon1, lat2, lon2]
-    )
-
-    # Calculate the distance using the haversine formula
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
-    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
-    c = 2 * math.asin(math.sqrt(a))
-    r = 6371
-    return c * r
-
-def flights_with_airports(flights_df, airports_df):
-    """
-    Join the flights_df and airports_df DataFrames to get
-    the latitude, longitude, and continent of the origin and destination airports.
-
-    :param flights_df: spark DataFrame
-    :param airports_df: spark DataFrame
-    :return: spark DataFrame
-    """
-
-    flight_airport_df = flights_df.join(
-        airports_df.alias("origin_airport"),
-        flights_df.origin_airport_iata == airports_df.iata
-    ).select(
-        flights_df["*"],
-        F.col("origin_airport.latitude").alias("origin_latitude"),
-        F.col("origin_airport.longitude").alias("origin_longitude"),
-        F.col("origin_airport.continent").alias("origin_continent")
-    )
-
-    flight_airport_df = flight_airport_df.join(
-        airports_df.alias("destination_airport"),
-        flights_df.destination_airport_iata == airports_df.iata
-    ).select(
-        flight_airport_df["*"],
-        F.col("destination_airport.latitude").alias("destination_latitude"),
-        F.col("destination_airport.longitude").alias("destination_longitude"),
-        F.col("destination_airport.continent").alias("destination_continent")
-    )
-
-    return flight_airport_df
-
-def get_airline_with_most_flights(flights_df, airlines_df):
+def get_airline_with_most_flights(flights_df):
     """
     Get the airline with the most flights.
 
@@ -84,17 +24,17 @@ def get_airline_with_most_flights(flights_df, airlines_df):
     :param airlines_df: spark DataFrame
     :return: str
     """
-    # Join the flights_df and airlines_df DataFrames
-    joined_df = flights_df.join(airlines_df, flights_df.airline_icao == airlines_df.ICAO)
+
     # Group by the airline name and count the number of flights
-    airline_flights = joined_df.groupBy("Name").count()
+    airline_flights = flights_df.groupBy("airline_name").count()
 
     # Get the airline with the most flights and the number of flights
     airline_with_most_flights = airline_flights.orderBy("count", ascending=False).first()
 
     return airline_with_most_flights
 
-def most_active_airline_per_continent(flights_df, airlines_df, airports_df):
+
+def get_most_active_airline_per_continent(flights_df):
     """
     Get the most active airline per continent.
 
@@ -104,18 +44,10 @@ def most_active_airline_per_continent(flights_df, airlines_df, airports_df):
     :return: dict
     """
 
-    # Join the flights_df and airports_df DataFrames
-    flights_df_airport_continent = flights_with_airports(flights_df, airports_df)   
-
-    # Join the flights_df and airlines_df DataFrames
-    flights_df_airlines = flights_df_airport_continent \
-                            .join(airlines_df, flights_df.airline_icao == airlines_df.ICAO) \
-                            .select(flights_df_airport_continent["*"], airlines_df.Name.alias("airline_name"))
-
     # Group by the airline name and continent and count the number of flights where the origin and destination continents
     # are the same
-    regional_airline_flights = flights_df_airlines \
-                                .filter(flights_df_airlines.origin_continent == flights_df_airlines.destination_continent) \
+    regional_airline_flights = flights_df \
+                                .filter(flights_df.origin_continent == flights_df.destination_continent) \
                                 .groupBy("airline_name", "origin_continent").count()
 
     # Get the most active airline per continent
@@ -127,7 +59,8 @@ def most_active_airline_per_continent(flights_df, airlines_df, airports_df):
 
     return {row["origin_continent"]: row["most_active_airline"] for row in most_active_airline_per_continent}
 
-def flight_with_longest_trajectory(flights_df, airports_df):
+
+def flight_with_longest_trajectory(flights_df):
     """
     Get the flight with the longest trajectory.
 
@@ -135,29 +68,92 @@ def flight_with_longest_trajectory(flights_df, airports_df):
     :param airports_df: spark DataFrame
     :return: dict
     """
-    # Join the flights_df and airports_df DataFrames
-    flights_df_airports = flights_with_airports(flights_df, airports_df)
-    
-    # define a distance udf
-    get_distance_udf = F.udf(get_distance, FloatType())
-
-    # Calculate the distance between the origin and destination airports
-    flights_df_airports = flights_df_airports.withColumn(
-        "distance",
-        get_distance_udf(
-            F.col("origin_latitude"),
-            F.col("origin_longitude"),
-            F.col("destination_latitude"),
-            F.col("destination_longitude")
-        )
-    )
 
     # Get the flight with the longest trajectory
-    longest_trajectory_flight = flights_df_airports.orderBy("distance", ascending=False).first()
+    longest_trajectory_flight = flights_df.orderBy("distance", ascending=False).first()
 
     return longest_trajectory_flight
 
-def average_flight_length_per_continent()
+
+def get_average_flight_length_per_continent(flights_df) -> dict:
+    """
+    Get the average flight length per continent.
+
+    :param flights_df: spark DataFrame
+    :return: dict
+    """
+
+    # filter out flights with different origin and destination continents
+    regional_flights = flights_df.filter(flights_df.origin_continent == flights_df.destination_continent)
+
+    # Group by the continent and calculate the average distance
+    average_flight_length_per_continent = regional_flights.groupBy("origin_continent") \
+                                            .agg(F.avg("distance").alias("average_distance")).collect()
+    
+    return {row["origin_continent"]: round(row["average_distance"], 2) for row in average_flight_length_per_continent}
+
+
+def get_top_three_aircraft_model_per_country(flights_df) -> dict:
+    """
+    Get the top three aircraft model per country.
+
+    :param flights_df: spark DataFrame
+    :return: dict
+    """
+
+    # Group by the aircraft model and country and count the number of flights
+    aircraft_model_flights = flights_df.groupBy("aircraft_code", "origin_country").count()
+
+    # Get the top three aircraft model per country
+    result = flights_df.groupBy('origin_country', 'aircraft_code') \
+                        .count() \
+                        .withColumn('rank', F.row_number().over(Window.partitionBy('origin_country').orderBy(F.desc('count')))) \
+                        .filter(F.col('rank') <= 3)
+
+    top_three_aircraft_model_per_country = result.groupBy("origin_country") \
+                                            .agg(F.concat_ws(", ", F.collect_list(result.aircraft_code)).alias("top_three_aircraft_model")) \
+                                            .collect()
+    
+    return {row["origin_country"]: row["top_three_aircraft_model"] for row in top_three_aircraft_model_per_country}
+
+
+# Get the airline with the most flights
+@app.command()
+def airline_with_most_flights():
+    airline_with_most_flights = get_airline_with_most_flights(flights_df)
+
+    print(f"The airline with the most flights is: {airline_with_most_flights.airline_name} with {airline_with_most_flights["count"]} flights")
+
+# # Get the most active airline per continent
+@app.command()
+def most_active_airline_per_continent():
+    most_active_airline_per_continent = get_most_active_airline_per_continent(flights_df)
+    print("The most active airline per continent is:")
+    for continent, airline in most_active_airline_per_continent.items():
+        print(f"{continent}: {airline}")
+
+# # Get the flight with the longest trajectory
+@app.command()
+def longest_trajectory_flight():
+    longest_trajectory_flight = flight_with_longest_trajectory(flights_df)
+    print("The flight with the longest trajectory is:", longest_trajectory_flight)
+
+# # Get the average flight length per continent
+@app.command()
+def average_flight_length_per_continent():
+    average_flight_length_per_continent = get_average_flight_length_per_continent(flights_df)
+    print("The average flight length per continent is:")
+    for continent, average_distance in average_flight_length_per_continent.items():
+        print(f"{continent}: {average_distance}")
+
+# # Get the top three aircraft model per country
+@app.command()
+def top_three_aircraft_model_per_country():
+    top_three_aircraft_model_per_country = get_top_three_aircraft_model_per_country(flights_df)
+    print("The top three aircraft model per country is:")
+    for country, aircraft_model in top_three_aircraft_model_per_country.items():
+        print(f"{country}: {aircraft_model}")
+
 
 if __name__ == "__main__":
     with open(os.path.join(os.pardir, 'config.json'), 'r') as f:
@@ -169,32 +165,11 @@ if __name__ == "__main__":
 
     spark = create_spark_session()
 
-    # Load the Airlines DataFrame
-    airlines_df = spark.read.option("header", "true").csv(airlines_path)
-
-    #Load the Airports DataFrame
-    airports_df = spark.read.option("header", "true").csv(airports_path)
-
     # Load the most recent Flights DataFrame
-    files = glob.glob(f"{flights_path}/*/*/*/*.csv")
+    files = glob.glob(f"{flights_path}/*/*/*/*.parquet")
     files.sort()
     most_recent_file = files[-1]
 
-    flights_df = spark.read.option("header", "true").csv(most_recent_file)
-
-    # Get the airline with the most flights
-    airline_with_most_flights = get_airline_with_most_flights(flights_df, airlines_df)
-
-    print(f"The airline with the most flights is: {airline_with_most_flights.Name} with {airline_with_most_flights["count"]} flights")
-
-    # Get the most active airline per continent
-    most_active_airline_per_continent = most_active_airline_per_continent(flights_df, airlines_df, airports_df)
-    print("The most active airline per continent is:")
-    for continent, airline in most_active_airline_per_continent.items():
-        print(f"{continent}: {airline}")
-
-    # Get the flight with the longest trajectory
-    longest_trajectory_flight = flight_with_longest_trajectory(flights_df, airports_df)
-    print("The flight with the longest trajectory is:", longest_trajectory_flight)
-
+    flights_df = spark.read.option("header", "true").parquet(most_recent_file)
+    app()
     spark.stop()
