5f485e8ddcc5fac0b0f7cce4641fde4ffb16b6af
GlobeCitizen <ahnich97@gmail.com>
2024-04-15 07:40:44 +0200

    create a config handler

---
+++ src/util/config_handler.py
@@ -0,0 +1,22 @@
+import configparser
+
+class ConfigHandler:
+    def __init__(self, config_file):
+        self.config = configparser.ConfigParser()
+        self.config.read(config_file)
+
+    def get_value(self, section, key):
+        try:
+            value = self.config.get(section, key)
+            return value
+        except (configparser.NoSectionError, configparser.NoOptionError):
+            return None
+
+    def set_value(self, section, key, value):
+        if not self.config.has_section(section):
+            self.config.add_section(section)
+        self.config.set(section, key, value)
+
+    def save_config(self, config_file):
+        with open(config_file, 'w') as file:
+            self.config.write(file)
