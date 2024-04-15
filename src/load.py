75930ae57e4a5cb21b077b010e892e159f8fc4b0
GlobeCitizen <ahnich97@gmail.com>
2024-04-15 14:51:28 +0200

    Use parquet instead of csv for flights df

--- src/load.py
+++ src/load.py
@@ -1,11 +1,11 @@
 from datetime import datetime
+import pytz
 from minio import Minio
 from prefect import task
-from pyspark.sql import SparkSession
 from pyspark.sql import DataFrame
 
 # @task()
-def save_df_to_csv(df: DataFrame, path: str, spark: SparkSession, client: Minio):
+def save_df_to_csv(df: DataFrame, path: str):
     """
     Save the dataFrame to a CSV file.
 
@@ -13,9 +13,8 @@ def save_df_to_csv(df: DataFrame, path: str, spark: SparkSession, client: Minio)
     :param spark: SparkSession
     :param path: csv file path
     """
-    # df.coalesce(1).write.option("header", "true").mode("overwrite").csv(path)
-    print("Airlines saved to CSV")
-
+    print(f"Saving DataFrame to {path}")
+    df.coalesce(1).write.option("header", "true").mode("overwrite").format("csv").save(path)
 
 # @task()
 def save_flights_to_parquet(flights_df: DataFrame, path: str, client: Minio):
@@ -28,7 +27,7 @@ def save_flights_to_parquet(flights_df: DataFrame, path: str, client: Minio):
     """
 
     # Get the current date and time
-    now = datetime.now()
+    now = datetime.now(pytz.timezone("Europe/Paris"))
 
     # Format the date and time as per the required format
     year = now.strftime("%Y")
@@ -37,20 +36,14 @@ def save_flights_to_parquet(flights_df: DataFrame, path: str, client: Minio):
     time = now.strftime("%H%M%S")
 
     # Create the file name
-    file_name = f"flights{year+month+day+time}.csv"
+    file_name = f"flights{year+month+day+time}.parquet"
 
     # Create the path to the Minio bucket
     date = f"year={year}/month={month}/day={day}/{file_name}"
     file_path = path + date
     path = f"s3a://exalt/{file_path}"
-
-    # Make the Minio bucket if it doesn't exist
-    if not client.bucket_exists("exalt"):
-        client.make_bucket("exalt")
-        print("Bucket created")
     
     # Save the DataFrame to a CSV file
-    flights_df.show()
-    flights_df.write.format("csv").mode("overwrite").save(path)
+    flights_df.write.format("parquet").mode("overwrite").save(path)
 
     print(f"Flights saved to {file_path}")
